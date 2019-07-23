85ba71f3d08707243f57ffd5c42b4f3fc88c11bb
alexsk88 <titoantifa69@gmail.com>
2019-07-22 20:50:17 -0500

    Se add method edit user

--- angularvideos/src/app/components/user-edit/user-edit.component.ts
+++ angularvideos/src/app/components/user-edit/user-edit.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/user';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-user-edit',
   templateUrl: './user-edit.component.html',
   styleUrls: ['./user-edit.component.css']
 })
-export class UserEditComponent implements OnInit {
+export class UserEditComponent implements OnInit
+{
+  public user: User;
+  public status: boolean;
+  public token: any;
+  public identity: any;
 
-  constructor() { }
+  constructor(private _userSV: UserService)
+  {
+    this.identity = _userSV.getIdentity();
+    this.token = _userSV.getToken();
 
-  ngOnInit() {
+    this.user = new User('',this.identity.name,this.identity.surname,
+    this.identity.email,'','ROLE_ADMIN','');
+  }
+
+  ngOnInit()
+  {
+  }
+
+  enviarEditUser(form:any)
+  {
+    this._userSV.edituser(this.user, this.token).subscribe(
+      res=>
+      {
+        this.status = true;
+        //console.log(res);
+        // Aqui SE PUEDE MEJORAR el res.status
+        this.identity.name = this.user.name;
+        this.identity.surname = this.user.surname;
+        this.identity.email = this.user.email;
+        localStorage.setItem('identity', JSON.stringify( this.identity))
+      },err=>{console.log(err);
+      }
+    )
   }
 
 }
--- angularvideos/src/app/services/user.service.ts
+++ angularvideos/src/app/services/user.service.ts
@@ -30,6 +30,18 @@ export class UserService
 
     return this._http.post(`${this.url}login`, params,{headers});
   }
+  
+  edituser(user:any, token): Observable<any>
+  {
+    let json = JSON.stringify(user);
+    let params = 'json='+json;
+    // console.log(params);
+    
+    let headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded')
+                                   .set('Authorization' , token) ;
+
+    return this._http.put(`${this.url}user/edit`, params,{headers});
+  }
 
   register(user: any): Observable<any>
   {
